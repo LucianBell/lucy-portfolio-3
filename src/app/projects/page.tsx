2a24b4715ffdc1f87cc217b26d4d14acf0b6ad84
Lucian Bellini <lucianbellini1@gmail.com>
2023-06-07 17:31:03 -0300

    Projects page header and sidebar added

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,5 +1,18 @@
-import React from "react";
+"use client";
+import { useState } from "react";
+import Header from "@/components/Header";
+import Sidebar from "@/components/Sidebar";
 
 export default function page() {
-  return <div>page</div>;
+  const [sidebarOpened, setSidebarOpened] = useState(false);
+
+  const openSidebar = () => setSidebarOpened(true);
+  const closeSidebar = () => setSidebarOpened(false);
+
+  return (
+    <>
+      <Header openSidebarClick={openSidebar} />
+      <Sidebar open={sidebarOpened} onClose={closeSidebar} />
+    </>
+  );
 }
