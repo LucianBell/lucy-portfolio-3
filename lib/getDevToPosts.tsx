0474d2f7e2758468b55b0dc9f557b09a6901b420
Lucian Bellini <lucianbellini1@gmail.com>
2023-06-11 21:26:18 -0300

    Finishing articles page config

--- lib/getDevToPosts.tsx
+++ lib/getDevToPosts.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default async function getDevToPosts() {
   const response = await fetch(
-    "https://dev.to/api/articles?username=lucianbellini&per_page=2"
+    "https://dev.to/api/articles?username=lucianbellini&per_page=6"
   );
 
   if (!response.ok) throw new Error("Failed to fetch data.");
--- src/app/articles/page.tsx
+++ src/app/articles/page.tsx
@@ -1,56 +1,63 @@
 import React from "react";
 import getDevToPosts from "../../../lib/getDevToPosts";
 import Link from "next/link";
+import { FaExternalLinkSquareAlt } from "react-icons/fa";
 
 export default async function page() {
   const articlesData: Promise<Article[]> = getDevToPosts();
 
   const articles = await articlesData;
 
-  const content =
-    (console.log(articles),
-    (
-      <section className=" ml-72 mr-8 grid grid-cols-3 grid-rows-3 gap-4">
-        {articles.map((article) => {
-          return (
-            <>
-              <div className=" bg-color-gray border border-color-light-gray rounded-lg">
-                <div className="">
-                  <img
-                    src={article.cover_image}
-                    alt="Project image"
-                    className=" rounded-lg grayscale transition-all duration-200 hover:filter-none"
-                  />
-                </div>
-                <div className=" p-6">
-                  <h2 className=" text-2xl">{article.title}</h2>
-                  <h3 className=" my-2">Made with:</h3>
-                  <p>{article.created_at}</p>
-                  <div className=" flex mt-3">
-                    <Link
-                      className=""
-                      href={article.canonical_url}
-                      target="_blank"
-                    ></Link>
-                  </div>
+  const content = (
+    <section className=" ml-72 mr-8 grid grid-cols-3 grid-rows-3 gap-4">
+      {articles.map((article) => {
+        const smallDate = (date: string) => {
+          const correctData = date.slice(0, 10);
+
+          return correctData;
+        };
+
+        return (
+          <>
+            <div className=" bg-color-gray border border-color-light-gray rounded-lg">
+              <div className="">
+                <img
+                  src={article.cover_image}
+                  alt="Project image"
+                  className=" rounded-lg grayscale transition-all duration-200 hover:filter-none"
+                />
+              </div>
+              <div className=" p-6">
+                <h2 className=" text-2xl">{article.title}</h2>
+                <p className=" my-2 italic">
+                  Posted at: {smallDate(article.published_at)}
+                </p>
+                <div className=" flex mt-3">
+                  <Link
+                    className=""
+                    href={article.canonical_url}
+                    target="_blank"
+                  >
+                    <div className=" group transition-all duration-150 px-3 py-1 rounded bg-white hover:bg-color-btn-gray">
+                      <p className=" transition-all duration-150 text-lg text-color-gray group-hover:text-white">
+                        Read article
+                      </p>
+                    </div>
+                  </Link>
                 </div>
               </div>
-            </>
-          );
-        })}
-      </section>
-    ));
+            </div>
+          </>
+        );
+      })}
+    </section>
+  );
 
   return (
     <>
-      <div className=" ml-80">
-        <h2>AAAAAAAA</h2>
-        <h1>My Articles</h1>
-      </div>
-
-      {console.log(articles[0])}
-
-      <div className=" ml-80">{[content]}</div>
+      <h2 className=" mx-72 text-xl">Let me show you some of my...</h2>
+      <h1 className=" mx-72 my-3 text-7xl">Articles</h1>
+      <div className=" flex">{[content]}</div>
     </>
   );
 }
