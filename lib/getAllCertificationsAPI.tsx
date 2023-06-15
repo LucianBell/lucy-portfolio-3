6585ceb69c632e46ab4474f6bc9317b4ddf5c040
Lucian Bellini <lucianbellini1@gmail.com>
2023-06-14 23:04:11 -0300

    Adding getAllCertifications lib

---
+++ lib/getAllCertificationsAPI.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default async function getAllCertificationsAPI() {
+  const response = await fetch(
+    "https://personal-api-v1.onrender.com/api/certifications",
+    { cache: "no-store" }
+  );
+
+  if (!response.ok) throw new Error("Failed to fetch data");
+
+  return response.json();
+}
