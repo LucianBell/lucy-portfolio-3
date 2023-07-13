aef37511203e888d439d9dc2649b7ffe40e11351
Lucian Bellini <lucianbellini1@gmail.com>
2023-07-12 22:32:28 -0300

    Updating hire-me page style

--- src/app/hire-me/page.tsx
+++ src/app/hire-me/page.tsx
@@ -23,106 +23,100 @@ export default function page() {
           <GiCheckMark className="  ml-4 pt-2 text-5xl" />
         </div>
 
-        <div className=" flex mb-4">
-          <div className=" mr-5">
-            <h3 className=" text-2xl">Ways of contacting me</h3>
-            <div className=" flex">
-              <div className=" group items-center">
-                <a
-                  href="mailto:lucianbellini1@gmail.com"
-                  className=" flex items-center"
-                >
-                  <MdEmail className=" transition-all duration-150 mr-1 group-hover:scale-110" />
-                  Email
-                </a>
-              </div>
-              <div className=" group items-center mx-4">
-                <a href="tel:05555999403500" className=" flex items-center">
-                  <IoCall className=" transition-all duration-150 mr-1 group-hover:scale-110" />
-                  Call me
-                </a>
-              </div>
-              <div className=" group items-center">
-                <a
-                  href="https://wa.me/5555999403500"
-                  target="_blank"
-                  className=" flex items-center"
-                >
-                  <BsWhatsapp className=" transition-all duration-150 mr-1 group-hover:scale-110" />
-                  Whatsapp
-                </a>
-              </div>
+        <div className="">
+          <h3 className=" text-2xl">Ways of contacting me</h3>
+          <div className=" flex">
+            <div className=" group items-center">
+              <a
+                href="mailto:lucianbellini1@gmail.com"
+                className=" flex items-center"
+              >
+                <MdEmail className=" transition-all duration-150 mr-1 group-hover:scale-110" />
+                Email
+              </a>
             </div>
-          </div>
-
-          <div>
-            <h3 className=" text-2xl">My social media</h3>
-            <div className=" flex items-baseline">
-              <a
-                href="https://www.linkedin.com/in/lucianbellini/"
-                className=" transition-all duration-150 mr-3 hover:scale-110"
-                target="_blank"
-              >
-                <BsLinkedin className=" text-lg mt-1" />
+            <div className=" group items-center mx-4">
+              <a href="tel:05555999403500" className=" flex items-center">
+                <IoCall className=" transition-all duration-150 mr-1 group-hover:scale-110" />
+                Call me
               </a>
+            </div>
+            <div className=" group items-center">
               <a
-                href="https://github.com/LucianBell"
-                className=" transition-all duration-150 mr-3 hover:scale-110"
+                href="https://wa.me/5555999403500"
                 target="_blank"
+                className=" flex items-center"
               >
-                <BsGithub className=" text-lg mt-1" />
-              </a>
-              <a
-                href="https://dev.to/lucianbellini"
-                className=" transition-all duration-150 mr-3 hover:scale-110"
-                target="_blank"
-              >
-                <MdOutlineLogoDev className=" text-lg mt-1" />
-              </a>
-              <a
-                href="https://www.youtube.com/channel/UCC8CbQPuKgjGm-QRUk8RICw"
-                className=" transition-all duration-150 mr-3 hover:scale-110"
-                target="_blank"
-              >
-                <BsYoutube className=" text-lg mt-1" />
-              </a>
-              <a
-                href="https://www.tiktok.com/@dev.lucy"
-                className=" transition-all duration-150 mr-3 hover:scale-110"
-                target="_blank"
-              >
-                <BsTiktok className=" text-lg mt-1" />
-              </a>
-              <a
-                href="https://www.instagram.com/lucianf.bellini/"
-                className=" transition-all duration-150 mr-3 hover:scale-110"
-                target="_blank"
-              >
-                <BsInstagram className=" text-lg mt-1" />
+                <BsWhatsapp className=" transition-all duration-150 mr-1 group-hover:scale-110" />
+                Whatsapp
               </a>
             </div>
           </div>
         </div>
 
-        <div>
-          <div className=" items-center flex">
-            <h3 className=" text-2xl mb-1">Need a closer look? I get it</h3>
-            <AiOutlineSmile className=" ml-4 text-3xl" />
-          </div>
-
-          <div className=" flex items-center group">
+        <div className=" my-7">
+          <h3 className=" text-2xl">My social media</h3>
+          <div className=" flex items-baseline">
             <a
-              href="https://docs.google.com/document/d/1hHY0NvO4qRalQ3x30Paegm3qhzgM_ubvFcI_21CjmGM/edit?usp=sharing"
+              href="https://www.linkedin.com/in/lucianbellini/"
+              className=" transition-all duration-150 mr-3 hover:scale-110"
               target="_blank"
-              className=" flex items-center"
             >
-              <BsFilePersonFill className=" transition-all duration-150 mr-1 group-hover:scale-110" />
-              My curriculum
+              <BsLinkedin className=" text-lg mt-1" />
+            </a>
+            <a
+              href="https://github.com/LucianBell"
+              className=" transition-all duration-150 mr-3 hover:scale-110"
+              target="_blank"
+            >
+              <BsGithub className=" text-lg mt-1" />
+            </a>
+            <a
+              href="https://dev.to/lucianbellini"
+              className=" transition-all duration-150 mr-3 hover:scale-110"
+              target="_blank"
+            >
+              <MdOutlineLogoDev className=" text-lg mt-1" />
+            </a>
+            <a
+              href="https://www.youtube.com/channel/UCC8CbQPuKgjGm-QRUk8RICw"
+              className=" transition-all duration-150 mr-3 hover:scale-110"
+              target="_blank"
+            >
+              <BsYoutube className=" text-lg mt-1" />
+            </a>
+            <a
+              href="https://www.tiktok.com/@dev.lucy"
+              className=" transition-all duration-150 mr-3 hover:scale-110"
+              target="_blank"
+            >
+              <BsTiktok className=" text-lg mt-1" />
+            </a>
+            <a
+              href="https://www.instagram.com/lucianf.bellini/"
+              className=" transition-all duration-150 mr-3 hover:scale-110"
+              target="_blank"
+            >
+              <BsInstagram className=" text-lg mt-1" />
             </a>
           </div>
         </div>
 
-        <h2>The Galery of Your Future Employee</h2>
+        <div className=" items-center flex">
+          <h3 className=" text-2xl">Need a closer look? I get it</h3>
+          <AiOutlineSmile className=" ml-4 text-3xl" />
+        </div>
+
+        <div className=" flex items-center group">
+          <a
+            href="https://docs.google.com/document/d/1hHY0NvO4qRalQ3x30Paegm3qhzgM_ubvFcI_21CjmGM/edit?usp=sharing"
+            target="_blank"
+            className=" flex items-center text-lg"
+          >
+            <BsFilePersonFill className=" transition-all duration-150 mr-1 group-hover:scale-110" />
+            My curriculum
+          </a>
+        </div>
       </section>
     </>
   );
