366a58120ba58aa57e632f7fce79b0b7ed86a213
Lucian Bellini <lucianbellini1@gmail.com>
2023-07-19 20:51:03 -0300

    Fix color error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
       <head>
         <link rel="icon" href="/icon.ico" sizes="any" />
       </head>
-      <body>{children}</body>
+      <body className=" bg-black text-white">{children}</body>
     </html>
   );
 }
